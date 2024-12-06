601346ec95c8acbaea299c2fab9594fb60a3c526
Suangga <suanggawija@gmail.com>
2024-12-06 13:06:40 +0800

    adding react-helmet

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { Benners, RecomendationFilm } from '../../components'
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
   return (
     <main>
+        <Helmet>
+          <title>TheMovie</title>
+          <meta
+            name="The Movie"
+            content="Trensings Movie, Film, and Amimation"
+          />
+        </Helmet>
         <Benners/>
         <RecomendationFilm/>
     </main>
