2ca2e003f531fad448625c1c60180d2156ad71b5
Suangga <suanggawija@gmail.com>
2024-12-06 11:00:53 +0800

    adding json-server update integrate API to Benners.jsx, CardFilm.jsx, RecomendationFilm.jsx

--- src/components/RecomendationFilm/RecomendationFilm.jsx
+++ src/components/RecomendationFilm/RecomendationFilm.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CardFilm from '../CardFilm/CardFilm'
-import MoanaImage from '../../assets/images/moana.webp'
 import MonsterUnivercityImage from '../../assets/images/thumb.webp'
-import InsideOut2 from '../../assets/images/insideOut2.webp'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const RecomendationFilm = () => {
+  const [post, setPost] = useState([]);
+  const navigate = useNavigate()
+
+  const handleDetailMovie = (id) => {
+    navigate(`/detailMovie/${id}`)
+  }
+
+  const getPostAPI = () => {
+    axios.get("http://localhost:3000/posts")
+      .then((res) => setPost(res.data))
+  }
+
+  useEffect(() => {
+    getPostAPI();
+  }, [])
   return (
     <div>
-        <div className='text-gray-200 text-xl font-semibold mt-10 mb-5'>Trending Movie</div>
-        <div className='grid grid-cols-5 gap-5'>
-            <CardFilm image={MonsterUnivercityImage} title={"Monster University"}/>
-            <CardFilm image={MoanaImage} title={"Moana"}/>
-            <CardFilm image={InsideOut2} title={"Inside Out 2"}/>
-            <CardFilm image={MonsterUnivercityImage} title={"Monster University"}/>
-            <CardFilm image={MoanaImage} title={"Moana"}/>
-            <CardFilm image={InsideOut2} title={"Inside Out 2"}/>
-            <CardFilm image={MonsterUnivercityImage} title={"Monster University"}/>
-            <CardFilm image={MoanaImage} title={"Moana"}/>
-            <CardFilm image={InsideOut2} title={"Inside Out 2"}/>
-            <CardFilm image={InsideOut2} title={"Inside Out 2"}/>
-        </div>
+      <div className="text-gray-200 text-xl font-semibold mt-10 mb-5">
+        Trending Movie
+      </div>
+      <div className="grid grid-cols-5 gap-5">
+        {post.map((post) => {
+          return (
+            <CardFilm
+              key={post.id}
+              id={post.id}
+              image={MonsterUnivercityImage} // URL gambar dari API
+              title={post.title}
+              detailMovie={handleDetailMovie}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default RecomendationFilm
